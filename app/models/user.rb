510f9f6836cff8c766a99e1fb1e5cffa03fc403b
Hoppingbird <hoppingbird@gmail.com>
2015-07-22 18:56:26 -0400

    markdown

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  has_many :wikis
+  has_many :wikis, dependent: :destroy
 
   #has_many :collaborators, through: :wikis
 
